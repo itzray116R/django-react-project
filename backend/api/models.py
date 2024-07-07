038d64b47de59c085a365d12af43cda5aecd01d8
itzray116R <rayhaanyaser@gmail.com>
2024-07-07 15:02:39 -0400

    Edit: models.py

--- backend/api/models.py
+++ backend/api/models.py
@@ -8,6 +8,6 @@ class Note(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="notes")
     
     
-    def __str__(self):
-        return str(self.title)
+def __str__(self):
+    return self.title
 
